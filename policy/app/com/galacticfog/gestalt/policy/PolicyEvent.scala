cd80a1190e18821d7f91d5738030c335c5fadb73
Eric Sonchaiwanich <eric@galacticfog.com>
2018-10-21 23:19:34 -0400

    timestmp: Long --> timestamp: String - workaround for issue #6

--- policy/app/com/galacticfog/gestalt/policy/PolicyEvent.scala
+++ policy/app/com/galacticfog/gestalt/policy/PolicyEvent.scala
@@ -5,7 +5,10 @@ import play.api.libs.json.{JsValue, Json}
 
 case class PolicyArgs( rule : ResourceInstance, payload : JsValue )
 
-case class PolicyEvent( id : String, identity : String, timestamp : Long, event : String, action : String, args : PolicyArgs )
+// ESS: Workaround for https://gitlab.com/galacticfog/gestalt-policy/issues/6 - error parsing timestamp field.  Converting to a String
+// Since the timestamp doesn't otherwise appear to be used in this codebase.
+//case class PolicyEvent( id : String, identity : String, timestamp : Long, event : String, action : String, args : PolicyArgs )
+case class PolicyEvent( id : String, identity : String, timestamp : String, event : String, action : String, args : PolicyArgs )
 
 object PolicyEvent {
   implicit val policyArgsFormat = Json.format[PolicyArgs]
